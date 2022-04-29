1236642b7327398d57a834a97d77cde8d51a5047
Leili_sdq <leila.sedighi.2020@gmail.com>
2022-04-29 19:13:14 +0430

    add button to change theme

--- app/src/main/java/com/example/styletraining/MainActivity.kt
+++ app/src/main/java/com/example/styletraining/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.styletraining
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.btn_switch_theme)
+        btn.setOnClickListener {
+            val isNightTheme = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+            changeTheme(isNightTheme)
+        }
+    }
+
+    private fun changeTheme(isNightTheme: Int) {
+        when (isNightTheme) {
+            Configuration.UI_MODE_NIGHT_YES ->
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            Configuration.UI_MODE_NIGHT_NO ->
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
     }
 }
